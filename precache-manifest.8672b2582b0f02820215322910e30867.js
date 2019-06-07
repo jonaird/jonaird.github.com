1480c4e4fa32692b2af7ae632eb93d745350fcd0
Jonathan Aird <jonathan.aird@gmail.com>
2019-06-07 14:10:33 -0500

    initial commit

---
+++ precache-manifest.8672b2582b0f02820215322910e30867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ff11791149fcb7fd08e",
+    "url": "/static/js/main.9ea1f2b6.chunk.js"
+  },
+  {
+    "revision": "b18970135b14472c42b5",
+    "url": "/static/js/2.3d64711d.chunk.js"
+  },
+  {
+    "revision": "4ff11791149fcb7fd08e",
+    "url": "/static/css/main.fe054a85.chunk.css"
+  },
+  {
+    "revision": "21f167cc4bee71a496ff34061ecb78b6",
+    "url": "/index.html"
+  }
+];
