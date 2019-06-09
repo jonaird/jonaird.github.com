da4abf522559de9c154ddaf10a6548e6236899f6
Jonathan Aird <jonathan.aird@gmail.com>
2019-06-08 19:30:35 -0500

    added vote price functionality

---
+++ precache-manifest.41712ef553210e1b5e19a1db9b24b8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc5a3110ce3ddaeacf87",
+    "url": "/static/js/main.22af83b2.chunk.js"
+  },
+  {
+    "revision": "8b1d7a6212b8a143292c",
+    "url": "/static/js/2.dbb0903c.chunk.js"
+  },
+  {
+    "revision": "fc5a3110ce3ddaeacf87",
+    "url": "/static/css/main.670d8e05.chunk.css"
+  },
+  {
+    "revision": "ab5f349274911d70e30a435a87ee0f78",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df8f56c169f71340ede0b87c6640ed58.js"
+  "/precache-manifest.41712ef553210e1b5e19a1db9b24b8e5.js"
 );
 
 workbox.clientsClaim();
