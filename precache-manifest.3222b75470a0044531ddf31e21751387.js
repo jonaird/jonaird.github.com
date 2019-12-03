e0ede465fedae849245bc6579bb48140828f6bc5
Jonathan Aird <jonathan.aird@gmail.com>
2019-12-03 11:37:40 +0200

    update

---
+++ precache-manifest.3222b75470a0044531ddf31e21751387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fef25b7d11c0ad79ef4b5de13dad052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "764850e4ce515672a044",
+    "url": "/static/css/main.63a8c686.chunk.css"
+  },
+  {
+    "revision": "08c194680851cd14166c",
+    "url": "/static/js/2.19154712.chunk.js"
+  },
+  {
+    "revision": "764850e4ce515672a044",
+    "url": "/static/js/main.a7d0a438.chunk.js"
+  },
+  {
+    "revision": "92ab7e1e0c11bc3772b3",
+    "url": "/static/js/runtime~main.c36707a9.js"
+  }
+]);
